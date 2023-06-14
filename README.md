82d05eb8142cb210fcc783d423c06d10b2223617
Flo <flovo89@hotmail.com>
2023-06-14 21:15:42 +0200

    add simple version for irrigation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # irrigation-script
+
+## irrigator-inout
+The idea here is to fill a bucket with water and use a sensor to detect high and low water level
+
+## irrigator-in
+Simple version to pump water for some time
---
+++ irrigator-in.py
@@ -0,0 +1,31 @@
+from actors.Motor import Motor
+from sensors.Sensor import Sensor
+import time
+
+pumpInA = Motor(2)
+pumpInB = Motor(3)
+sensorEmpty = Sensor(21)
+
+def isEmpty():
+    return sensorEmpty.getState()
+
+def pumpWater(motor, time_s):
+    count = 0
+    motor.setState(True)
+
+    while count < time_s:
+        count = count + 1
+        if isEmpty():
+            break
+        time.sleep(1)
+
+    motor.setState(False)
+
+
+#########################################################
+# Main flow
+#########################################################
+pumpWater(pumpInA, 15)
+pumpWater(pumpInB, 15)
+
+
